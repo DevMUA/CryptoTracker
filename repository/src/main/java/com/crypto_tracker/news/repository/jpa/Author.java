53f1b4301f723c66a0330881cab2e295b606c536
jacintoluf <jacinto.luf@gmail.com>
2021-06-21 00:08:26 +0100

    jpa news fix

--- repository/src/main/java/com/crypto_tracker/news/repository/jpa/Author.java
+++ repository/src/main/java/com/crypto_tracker/news/repository/jpa/Author.java
@@ -1,14 +1,11 @@
 package com.crypto_tracker.news.repository.jpa;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
 public class Author {
     @Id
-    @GeneratedValue
-    private Long id;
     private String name;
 
     public Author(String name) {
@@ -25,14 +22,5 @@ public class Author {
 
     public String getName() {
         return name;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    @Id
-    public Long getId() {
-        return id;
     }
 }
